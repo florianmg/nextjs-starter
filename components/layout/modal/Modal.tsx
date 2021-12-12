db52161cd9de3dcfbdd11e21d6811c242a1d6a84
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-12-12 18:28:14 +0100

    feat: modal component structure

--- components/layout/modal/Modal.tsx
+++ components/layout/modal/Modal.tsx
@@ -1,7 +1,23 @@
 import styles from './Modal.module.scss';
+import { IoClose } from 'react-icons/io5';
+interface IModal {
+  isOpen: boolean;
+  closeModal: () => void;
+}
 
-const Modal: React.FC = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+const Modal: React.FC<IModal> = ({ children, isOpen, closeModal }) => {
+  if (!isOpen) return <></>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__content}>
+        <IoClose
+          onClick={closeModal}
+          className={styles.container__content__icon}
+        />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
