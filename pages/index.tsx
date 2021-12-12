d0bc72219107e5336a916e849eb37b71c3c65de3
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-12-12 14:38:34 +0100

    feat: configure default i18n

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
+import { useTranslation } from 'react-i18next';
 
 const Home: NextPage = () => {
+  const { t } = useTranslation();
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +16,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">{t('app_name')}</a>
         </h1>
 
         <p className={styles.description}>
