4f1ff9f5eb7817a492b54205d04f1c6f76a8aa50
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-12-13 07:56:29 +0100

    feat: default css variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/reset.css';
 import '../styles/globals.scss';
+import '../styles/variables.scss';
 import '../i18n';
 import type { AppProps } from 'next/app';
 import { Navbar } from '../components/layout';
@@ -7,7 +8,7 @@ import { Navbar } from '../components/layout';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar isLogged />
+      <Navbar isLogged={true} />
       <Component {...pageProps} />
     </>
   );
