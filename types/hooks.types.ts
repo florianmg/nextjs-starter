e303234bf16e988a6c5a7c8ee0b744e1d81559d5
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-12-16 07:47:32 +0100

    feat: create hook types

---
+++ types/hooks.types.ts
@@ -0,0 +1,19 @@
+import IUser from './user.types';
+
+export interface IUseAuthProps {
+  secure?: boolean;
+  onCheckAuthStateSuccess?: () => void;
+}
+
+export interface IUseAuth {
+  user: IUser | undefined;
+  googleAuthenticate: () => void;
+  emailRegister: ({ email, password }: IEmailLogInfos) => void;
+  emailLogin: ({ email, password }: IEmailLogInfos) => void;
+  logout: () => void;
+}
+
+export interface IEmailLogInfos {
+  email: string;
+  password: string;
+}
