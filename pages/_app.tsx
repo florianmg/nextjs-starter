afa50fbf95660b77f4de599c071a0fe0b7660a84
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-12-17 08:55:57 +0100

    fix: remove old translations stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/reset.css';
 import '../styles/globals.scss';
 import '../styles/variables.scss';
-import '../i18n';
 import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
