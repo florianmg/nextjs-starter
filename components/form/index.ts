60b850271e58dc9f59cccc9b31e4eae42482655b
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-12-18 15:28:53 +0100

    feat: create specific component for google auth button

--- components/form/index.ts
+++ components/form/index.ts
@@ -2,5 +2,6 @@ import InputText from "./InputText";
 import Button from "./Button";
 import Checkbox from "./Checkbox";
 import Select from "./Select";
+import GoogleAuthenticateButton from "./GoogleAuthenticateButton";
 
-export { InputText, Button, Checkbox, Select }
+export { InputText, Button, Checkbox, Select, GoogleAuthenticateButton }
